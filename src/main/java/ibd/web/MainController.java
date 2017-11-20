16d6fdea072667c8263aa056e5ddec61f5c38096
Devarashi <212466@student.pwr.edu.pl>
2017-11-20 21:36:14 +0100

    Welcome page

---
+++ src/main/java/ibd/web/MainController.java
@@ -0,0 +1,22 @@
+package ibd.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class MainController {
+
+    @GetMapping("")
+    public String homePage(){
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String hello(){
+        return "home";
+    }
+
+}
