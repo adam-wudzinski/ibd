ac0de820eaa91fcc6ca7ecae1da63242668d1ecf
Devarashi <212466@student.pwr.edu.pl>
2017-11-21 02:38:01 +0100

    Logout

--- src/main/java/ibd/web/MainController.java
+++ src/main/java/ibd/web/MainController.java
@@ -1,10 +1,16 @@
 package ibd.web;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/")
@@ -29,5 +35,14 @@ public class MainController {
     public String loginError(Model model) {
         model.addAttribute("loginError", true);
         return "auth/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login";
     }
 }
