4f6d8ad80d14b23cd6dc4f5c415c373eea016c03
Devarashi <212466@student.pwr.edu.pl>
2017-11-21 10:49:31 +0100

    Version 1.0

--- src/main/java/ibd/config/WebSecurityConfig.java
+++ src/main/java/ibd/config/WebSecurityConfig.java
@@ -15,9 +15,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().permitAll()
-           //     .antMatchers("/").permitAll()
-          //      .anyRequest().authenticated()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .formLogin()
                 .loginPage("/login")
